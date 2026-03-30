f5541b651a0ed24f5528283dc1a678856766522b
Ankarmin <davidspr.22@gmail.com>
2026-03-30 01:21:30 -0500

    Sección 3 terminada

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+  private cars = [
+    { id: 1, make: 'Toyota', model: 'Camry', year: 2020 },
+    { id: 2, make: 'Honda', model: 'Civic', year: 2019 },
+    { id: 3, make: 'Ford', model: 'Mustang', year: 2021 },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+
+    if (!car) throw new NotFoundException(`Car with ID ${id} not found`);
+
+    return car;
+  }
+}
